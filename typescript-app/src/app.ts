84fb5011df252f8f75a9a836aba51d18aa30179c
murraybill <thorsten.voscak@gmx.de>
2021-11-18 23:53:00 +0100

    typescript kram

---
+++ typescript-app/src/app.ts
@@ -0,0 +1,14 @@
+interface Todo{
+    description: string;
+    status: string;
+}
+
+const todo: Todo = {
+    description: "TypeScript lernen",
+    status: "open",
+}
+
+
+let text = "Hallo Welt";
+
+console.log(text);
