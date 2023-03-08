27a9fa2f697788be2eec10eb1bb3b54d1b9eb3e8
murraybill <thorsten.voscak@gmx.de>
2023-03-08 22:38:50 +0100

    Header Footer Test

--- murraybills-app/routes/about.tsx
+++ murraybills-app/routes/about.tsx
@@ -1,10 +1,13 @@
 // routes/about.tsx
 
+
 export default function AboutPage() {
     return (
+      <>
       <main>
         <h1>About</h1>
         <p>This is the about page.</p>
       </main>
+      </>
     );
   }
--- murraybills-app/routes/index.tsx
+++ murraybills-app/routes/index.tsx
@@ -1,18 +1,13 @@
+import Header from "../components/Header.tsx"
+import Footer from "../components/Footer.tsx"
+import ImageExample from "../components/imageExample.tsx"
 
-export default function Home() {
+export default function home() {
   return (
-    <div class="p-4 mx-auto max-w-screen-md">
-      <img
-        src="/logo.svg"
-        class="w-32 h-32"
-        alt="the fresh logo: a sliced lemon dripping with juice"
-      />
-      <h2>Current time: {new Date().toLocaleString()}</h2>
-      <img src="/example_pic.jpeg"
-        class="w-80"
-        alt=""
-      />
-    
-    </div>
+      <>
+      <Header/>
+      <ImageExample/>
+      <Footer/>      
+      </>
   );
 }
